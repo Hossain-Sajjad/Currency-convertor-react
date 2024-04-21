320cbbd966e6ebe038c82c4b28f082b2aaeab475
Hossain-Sajjad <sunshinems58@gmail.com>
2024-04-22 03:15:23 +0600

    converter box created

--- src/components/CurrencyConvertor.jsx
+++ src/components/CurrencyConvertor.jsx
@@ -1,7 +1,43 @@
-const CurrencyConvertor = () => {
-  return (
-    <div>CurrencyConvertor</div>
-  )
-}
+import { useState } from "react";
 
-export default CurrencyConvertor
+const CurrencyConvertor = () => {
+  const [currencies, setCurrencies] = useState([]);
+  const [amount, setAmmount] = useState(1);
+
+  // currencies -> https://api.frankfurter.app/currencies
+
+  // Conversion -> https://api.frankfurter.app/latest?amount=1&from=USD&to=INR
+
+  return (
+    <div className='max-w-xl mx-auto my-10 p-5 bg-white rounded-lg shadow-md'>
+      <h2 className='mb-5 text-2xl font-semibold text-gray-700'>
+        Currency Convertor
+      </h2>
+      <div>Dropdowns</div>
+      <div className='mt-4 '>
+        <label
+          htmlFor='ammount'
+          className='block text-sm font-medium text-gray-700'
+        >
+          Amount
+        </label>
+        <input
+          value={amount}
+          onChange={(e) => setAmmount(e.target.value)}
+          type='number'
+          className='w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 mt-1'
+        />
+      </div>
+      <div className='flex justify-end mt-6'>
+        <button className='px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'>
+          Convert
+        </button>
+      </div>
+      <div className='mt-4 text-lg font-medium text-right text-green-600'>
+        Converted Amount: 123
+      </div>
+    </div>
+  );
+};
+
+export default CurrencyConvertor;
